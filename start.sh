2208a7b91a2e94db79f160610133603cd0a29e85
tompng <tomoyapenguin@gmail.com>
2022-03-02 22:41:41 +0900

    add default server

--- start.sh
+++ start.sh
@@ -1,14 +1,15 @@
 #! /bin/sh
 
-# https://qiita.com/manontroppo1974/items/78f4b1b8d81694f9fd4a
+# https://github.com/Pugmatt/BedrockConnect
 domains="
   geo.hivebedrock.network
-  play.galaxite.net
+  hivebedrock.network
   mco.mineplex.com
-  mco.cubecraft.net
-  play.pixelparadise.gg
-  mco.lbsg.net
   play.inpvp.net
+  mco.lbsg.net
+  mco.cubecraft.net
+  play.galaxite.net
+  play.pixelparadise.gg
 "
 
 ip=$(ifconfig | grep "inet " | tail -n1 | cut -d" " -f2)
@@ -16,5 +17,8 @@ a='local-zone: "'
 b='." redirect\nlocal-data: "'
 c=" A $ip\""
 echo "$domains" | sed -r "s/ *([^ ]+)/$a\1$b\1$c/g" > a-records.conf
+
+minecraft_port=${1:-19133}
+printf '[{"name":"%s:%d","iconUrl":"","address":"%s","port":%d}]' "$ip" "$minecraft_port" "$ip" "$minecraft_port" > custom_servers.json
 
 docker-compose up
