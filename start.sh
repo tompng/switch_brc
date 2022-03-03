00b37fdc92e04edd936d19221510e37684282e49
tompng <tomoyapenguin@gmail.com>
2022-03-03 22:09:53 +0900

    tunneling

--- start.sh
+++ start.sh
@@ -1,5 +1,19 @@
 #! /bin/sh
 
+usage_message="usage:\n$0 client [server_name]\n$0 server [server_name] [minecraft_port]"
+if ! (echo "$1 $2 $3" | grep -E '^(server [a-zA-Z0-9_\.-]+ \d+|client [a-zA-Z0-9_\.-]+ )$' > /dev/null); then
+  echo "$usage_message"
+  exit
+elif [ "$1" = server ] && { test "$3" -eq 19131 || test "$3" -eq 19132; }; then
+  echo "port reserved"
+  echo "$usage_message"
+  exit
+fi
+
+tunnel_mode="$1"
+server_name="$2"
+minecraft_port="${3:-19131}"
+
 # https://github.com/Pugmatt/BedrockConnect
 domains="
   geo.hivebedrock.network
@@ -13,12 +27,21 @@ domains="
 "
 
 ip=$(ifconfig | grep "inet " | tail -n1 | cut -d" " -f2)
+echo "Your IP: $ip"
+
 a='local-zone: "'
 b='." redirect\nlocal-data: "'
 c=" A $ip\""
 echo "$domains" | sed -r "s/ *([^ ]+)/$a\1$b\1$c/g" > a-records.conf
 
-minecraft_port=${1:-19133}
 printf '[{"name":"%s:%d","iconUrl":"","address":"%s","port":%d}]' "$ip" "$minecraft_port" "$ip" "$minecraft_port" > custom_servers.json
 
-docker-compose up
+docker compose stop
+docker compose up -d
+docker compose start unbound bedrock-connect wrtc_tunnel
+
+if [ "$tunnel_mode" = server ]; then
+  docker compose exec wrtc_tunnel node wrtc_tunnel/dist/main.js server "$server_name" gateway.docker.internal "$minecraft_port"
+else
+  docker compose exec wrtc_tunnel node wrtc_tunnel/dist/main.js client "$server_name" "$minecraft_port"
+fi
